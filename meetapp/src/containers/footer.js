605f093316d5e724de2d489c544b8bf58860f14b
kartiks1309 <kartik.s1309@gmail.com>
2022-04-13 12:57:20 +0530

    bug fix in footer.js

--- meetapp/src/containers/footer.js
+++ meetapp/src/containers/footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Footer from '../components/Footer'
+import Footer from '../components/footer'
 import Icon from '../components/icons'
 
 export function FooterContainer() {
