0af0b3c95324bda2fc5350a47f2da49c5744b9c1
Kajal <kajalkaushal700@gmail.com>
2022-04-14 15:34:02 +0530

    removed unwanted sty=uff

--- meetapp/src/index.js
+++ meetapp/src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { GlobalStyles } from './global-styles';
-import App from './App';
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import Card from "./components/Cards/Cards.js"
-// import Card from "./components/Cards/Cards.js"
-
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
-    <>
-    {/* <GlobalStyles /> */}
-    <App />
-    </>
-    , document.getElementById('root'));
+  <App />,
+
+  document.getElementById("root")
+);
