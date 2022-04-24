de1644c5b162c57864021503bfe23f32c6d7503c
PRATYUSH RANJAN <76619629+Pratyush7901@users.noreply.github.com>
2022-04-24 21:35:58 +0530

    Cahneges to Faq page

--- meetapp/src/pages/FAQ.js
+++ meetapp/src/pages/FAQ.js
@@ -1,20 +1,31 @@
 import React from "react";
 import questions from "./questions.js";
 import Banner from "./Banner";
- function Faq() {
+import Navbar from "../components/Navbar/Navbar"
+import { FooterContainer } from "../containers/footer";
+function Faq() {
   return (
-    <Banner>
-      <Banner.Header>Frequently Asked Questions</Banner.Header>
-      {questions.map((question) => (
-        <Banner.Entity key={question.id}>
-          <Banner.Question>{question.question}</Banner.Question>
-          <Banner.Text>{question.answer}</Banner.Text>
-        </Banner.Entity>
-      ))}
-      <h4>
-        Question not on the list? Contact us for further enquiries
-      </h4>
-    </Banner>
+    <div>
+      <div><Navbar /></div>
+      <div>
+        <Banner>
+          <Banner.Header>Frequently Asked Questions</Banner.Header>
+          {questions.map((question) => (
+            <Banner.Entity key={question.id}>
+              <Banner.Question>{question.question}</Banner.Question>
+              <Banner.Text>{question.answer}</Banner.Text>
+            </Banner.Entity>
+          ))}
+          <h4>
+            Question not on the list? Contact us for further enquiries
+          </h4>
+        </Banner>
+      </div>
+      <div>
+        <FooterContainer />
+      </div>
+    </div>
+
     //<h1>Hello World</h1>
   );
 }
